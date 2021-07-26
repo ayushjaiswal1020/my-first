2ee4d3eb63a439b09fb9d041e0c6cb188e26195f
Ayush Jaiswal <69688537+ayushjaiswal1020@users.noreply.github.com>
2021-07-26 11:48:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,10 @@
 from os import environ
 from pyrogram import Client, filters
 
-API_ID = environ.get('API_ID')
-API_HASH = 6e679b44365774a91173a9c45cac3b2a
-BOT_TOKEN = environ.get('BOT_TOKEN')
+
 
 bot = Client('test bot',
-             api_id=3646731,
-             api_hash=API_HASH,
+             
              bot_token=1785476005:AAEoPNc1KjLf_TDquxSjyPgutN2dbZag2EE,
              workers=50,
              sleep_threshold=10)
